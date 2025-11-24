7d0cf4c3d8fff71913357ac87c126618c94b5dc7
Arianna2109 <ariannapeguero.21@gmail.com>
2025-11-24 18:14:06 +0000

    I finally did it!! Yayyyy

--- part2.py
+++ part2.py
@@ -1,1 +1,18 @@
+choice = input("Welcome to Quick Math Calculater! Do you want to do addition or subtraction?")
 
+if choice == "addition":
+    print("You chose addition.")
+    num1 = int(input("Enter first number: "))
+    num2 = int(input("Enter second number: "))
+    sum = num1 + num2
+    print("The answer is:" + str(sum))
+
+elif choice == "subtraction":
+    print("You chose subtraction")
+    num1 = int(input("Enter first number: "))
+    num2 = int(input("Enter second number: "))
+    sum = num1 - num2
+    print("The answer is:" + str(sum))
+
+else:
+    print("Please choose either addition or subtraction.")
